15b29cd41a162f966d0fe0585daf7b0dda51bc34
fujidaiti <fujidaiti@gmail.com>
2025-11-20 18:27:37 +0900

    e2e: remove containsLintError matcher and its tests; use containsLintErrors/containsAnyLintErrors

--- e2e/src/matchers.dart
+++ e2e/src/matchers.dart
@@ -2,27 +2,6 @@ import 'package:test/test.dart';
 
 import 'analyzer_output.dart';
 
-/// Matcher that checks if analyzer output contains a specific lint error
-Matcher containsLintError({
-  required String file,
-  int? line,
-  int? col,
-  Matcher? message,
-}) {
-  return predicate<AnalyzerOutput>(
-    (output) {
-      return output.errors.any(
-        (error) =>
-            error.file == file &&
-            (line == null || error.diagnostic.line == line) &&
-            (col == null || error.diagnostic.col == col) &&
-            (message == null || message.matches(error.diagnostic.message, {})),
-      );
-    },
-    'contains lint error in $file${line != null ? ':$line' : ''}${col != null ? ':$col' : ''}',
-  );
-}
-
 /// Matcher that checks if analyzer output contains any lint errors for a file.
 ///
 /// If [lines] is null or empty, it matches when any error exists for [file].
